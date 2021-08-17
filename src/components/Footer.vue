387499cc26ee4088940e2bb8f602f82e57c86a26
dendydandees <dendydharmawanq@gmail.com>
2021-08-17 09:23:17 +0700

    fix footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,5 @@
 <template>
-  <footer class="text-center text-sm p-4 text-gray-200 bg-gray-600 fixed bottom-0 w-full">
+  <footer class="text-center text-sm p-4 text-gray-200 bg-gray-600 absolute bottom-0 w-full">
     <p class="inline-block">&copy; {{ new Date().getFullYear() }} Instant Whastapp &#183; by</p>
     <a
       href="https://dendydharmawan.thedev.id"
